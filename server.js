9d55b3f5f6f0503ff068a107b3e78bfa73630ead
MarkB28Jr <mark.brashear265@gmail.com>
2024-05-07 09:07:32 -0500

    Init push

---
+++ server.js
@@ -0,0 +1,17 @@
+require ("dotenv").config()
+require("./config/db.connection")
+
+const { PORT } = process.env
+const express = require("express")
+const app = express()
+const cors = require('cors')
+const morgan = require('morgan')
+// const peopleRouter = require('./routes/people')
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.use(cors())
+app.use(morgan('dev'))
+// app.use('/people', peopleRouter)
+
+app.listen(PORT, ()=> console.log(`Connected to ${PORT}!`))
