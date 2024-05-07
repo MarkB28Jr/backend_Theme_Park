ba90e42cf81961cc28dae585959cfb86002568f5
Eric Fargo <fargo10k@gmail.com>
2024-05-07 10:36:52 -0400

    routes set

---
+++ routes/parks.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+
+const parkRouter = require('../controllers/parks')
+
+
+router.get('/', parkRouter.index)
+router.post('/', parkRouter.create)
+router.get('/:id', parkRouter.show)
+
+
+module.exports = router
