5e5be763f194f0f437d97d9aee6ab60cdf8374e0
eliaswnoel <eliaswnoel@gmail.com>
2024-05-07 10:56:22 -0400

    models

---
+++ models/park.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+const Schema  = mongoose.Schema
+
+const ParkSchema = new Schema ({
+    name: String,
+    image: String,
+    title: String,
+}, {timestamps: true});
+
+module.exports = mongoose.model("Park", ParkSchema);
