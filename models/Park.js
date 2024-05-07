0c541b9901fcdb36125d9c39b303e58e98e03335
MarkB28Jr <mark.brashear265@gmail.com>
2024-05-07 12:32:59 -0500

    Created Model for theme park

--- models/Park.js
+++ models/Park.js
@@ -1,10 +1,27 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+ 
+const RideSchema = new Schema({
+    name: String,
+    image: String,
+    ride: {
+        type: String,
+        enum: ['Roller Coaster', 'Bumper Cars', 'Bungee  Jumps', 'Ferris Wheel', 'Tilt-A-Whir'],
+    }
+}, { timestamps: true
+})
+
+const FoodSchema = new Schema({
+    name: String,
+    description: String,
+}, { timestamps: true
+})
 
 const ParkSchema = new Schema({
     name: String,
     image: String,
-    title: String,
+    rides: [RideSchema],
+    food: [FoodSchema]
 }, { timestamps: true });
 
 module.exports = mongoose.model("Park", ParkSchema);
