03cf7778c6c6f27f2075f5b3f95a1a3f5254d8e1
MarkB28Jr <mark.brashear265@gmail.com>
2024-05-08 08:51:28 -0500

    Changed Schema

--- models/Park.js
+++ models/Park.js
@@ -1,27 +1,14 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
  
-const RideSchema = new Schema({
-    name: String,
-    image: String,
-    ride: {
-        type: String,
-        enum: ['Roller Coaster', 'Bumper Cars', 'Bungee  Jumps', 'Ferris Wheel', 'Tilt-A-Whir'],
-    }
-}, { timestamps: true
-})
-
-const FoodSchema = new Schema({
-    name: String,
-    description: String,
-}, { timestamps: true
-})
-
 const ParkSchema = new Schema({
     name: String,
     image: String,
-    rides: [RideSchema],
-    food: [FoodSchema]
+    rides: {
+        type: String,
+        enum: ['Roller Coaster', 'Bumper Cars', 'Bungee  Jumps', 'Ferris Wheel', 'Tilt-A-Whir'],
+    },
+    food: String,
 }, { timestamps: true });
 
 module.exports = mongoose.model("Park", ParkSchema);
