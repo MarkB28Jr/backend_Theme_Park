bb98ce8622b7bd066522a139d34f97357035b8bb
MarkB28Jr <mark.brashear265@gmail.com>
2024-05-08 09:37:43 -0500

    updating heroku

--- server.js
+++ server.js
@@ -13,5 +13,10 @@ app.use(express.json())
 app.use(cors())
 app.use(morgan('dev'))
 app.use('/park', parkRouter)
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 app.listen(PORT, () => console.log(`Connected to ${PORT}!`))
