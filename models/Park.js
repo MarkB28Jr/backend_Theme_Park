fbf71fdf886c6158e647b07228f86692374eb3c4
MarkB28Jr <mark.brashear265@gmail.com>
2024-05-08 13:40:20 -0500

    working on getting it working and fixing Schema

--- models/Park.js
+++ models/Park.js
@@ -4,10 +4,10 @@ const Schema = mongoose.Schema
 const ParkSchema = new Schema({
     name: String,
     image: String,
-    rides: [{
+    rides: {
         type: String,
         enum: ['Roller Coaster', 'Bumper Cars', 'Bungee  Jumps', 'Ferris Wheel', 'Tilt-A-Whir'],
-    }]
+    }
 }, { timestamps: true });
 
 module.exports = mongoose.model("Park", ParkSchema)
