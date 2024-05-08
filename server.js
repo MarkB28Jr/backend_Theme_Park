e66e32597707abf4ebe25681c120e82e0a77e005
MarkB28Jr <mark.brashear265@gmail.com>
2024-05-08 09:53:16 -0500

    fixing stuff

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use(express.json())
 app.use(cors())
 app.use(morgan('dev'))
 app.use('/park', parkRouter)
+
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
